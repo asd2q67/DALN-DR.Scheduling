db1d86d4057350e761d000a25580f4c0cfbaa133
Lê Tiến Hưng <101103226+Shoorei@users.noreply.github.com>
2023-10-05 16:50:04 +0700

    Update dr_detail.php

--- src/php/dr_detail.php
+++ src/php/dr_detail.php
@@ -12,38 +12,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 if ($result = $mysqli-> query("SELECT * FROM dr_detail")) {
 
-if(mysqli_num_rows($result)>0){
-
-?>
-	<table border="1">
-	<tr>
-		<th>id</th>
-		<th>TITLE</th>
-		<th>CONTENT</th>
-		<th>DATE</th>
-		<th><a href="news.php">Add</a></th>
-	</tr>
-	
-<?php 
-/*
-	while($row = mysqli_fetch_assoc($result)){
-?>
-	<tr>
-		<td><?php echo $row["id"]  ?> </td>
-		<td> <?php echo $row["title"] ?> </td>
-		<td> <?php echo $row["content"] ?> </td>
-		<td> <?php echo $row["date"]  ?></td>
-		<td><a href="update.php?id=<?php echo $row['id']; ?>">edit</a> | <a href="delete.php?id=<?php echo $row['id']; ?>">remove</a></td>
-	</tr>
-
-	<?php
-	}
-}
-else{
-	echo "Không có dữ liệu";
-}
-}
-*/
+if(mysqli_num_rows($result)>0){	
 
 if ($method == 'GET') {
 	if (!$id) echo '[';
