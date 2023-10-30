413810ff0d05bcd98c1e81be9eda9d9390a5f5bf
Hoa Thi Thu Trang <thutranghoa@Hoas-MacBook-Air.local>
2023-10-30 08:46:56 +0700

    new

--- schedule/Data.py
+++ schedule/Data.py
@@ -8,20 +8,17 @@ from Demand import Demand
 
 class Data:
 
-    def __init__ (self, l_doctors : List['Doctor'], l_rooms : List['Room'] , day_off , day_ol , room_ol, l_demands : List['Demand']):
+    def __init__ (self, l_doctors : List['Doctor'], l_rooms : List['Room'] , day_off , day_ol , room_ol):
         self.l_doctors  = l_doctors
         self.l_rooms= l_rooms
         self.day_off = day_off
         self.day_ol = day_ol
         self.room_ol = room_ol
-        self.horizon = 7
-        self.l_demands= l_demands
+        self.horizon = 14
+        # self.workLoad= workload
         
     def get_num_rooms (self):
         return len(self.l_rooms)
-    
+
     def get_num_doctors (self):
         return len(self.l_doctors)
-
-    def get_num_demands (self):
-        return len(self.l_demands)
